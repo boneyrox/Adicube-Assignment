f818ede8d22b737d527cd2256bb7fececb741a24
mj1610 <mukulbenara@gmail.com>
2021-06-08 13:31:16 +0530

    model adicube

---
+++ src/Frontend/Brand.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Brand = () => {
+    return (
+        <div>
+            <h1>This is Brand form page</h1>
+        </div>
+    );
+};
+
+export default Brand;
---
+++ src/Frontend/Influencer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Influencer = () => {
+    return (
+        <div>
+            <h1>This will be Influencer form page</h1>
+        </div>
+    );
+};
+
+export default Influencer;
